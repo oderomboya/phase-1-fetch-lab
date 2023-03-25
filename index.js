4f92e909e0fbde7adf93fc708caedefaba2cd89f
oderomboya <davismboya3@gmail.com>
2023-03-25 22:23:09 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
+
+  function fetchBooks() {
+    // To pass the tests, don't forget to return your fetch!
   
-}
-
-function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
+    return fetch("https://anapioficeandfire.com/api/books")
+      .then((response) => response.json())
+      .then((json) => renderBooks(json));
+  }
+  
+  function renderBooks(books) {
+    const main = document.querySelector("main");
+    books.forEach((book) => {
+      const h2 = document.createElement("h2");
+      h2.innerHTML = book.name;
+      main.appendChild(h2);
+    });
+  }
+  
+  document.addEventListener("DOMContentLoaded", function () {
+    fetchBooks();
   });
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
